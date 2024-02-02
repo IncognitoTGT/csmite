98c38f062797291f20d2991e98cbccf1020ac925
incognitotgt <mashoor539@icloud.com>
2024-02-02 12:26:54 -0500

    mirror info site

--- dlsrv/server.ts
+++ dlsrv/server.ts
@@ -24,12 +24,6 @@ app.get("/:image", (req, res) => {
   res.set("Content-Type", "application/octet-stream");
   res.download(`/images/${req.params.image}`);
 });
-app.get("/", (_req, res) => {
-  res.set("Content-Type", "text/html");
-  res.send(`
-  <!DOCTYPE html><html><script>window.location.href = "https://asyncsmasher.com"</script></html>
-  `);
-});
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
