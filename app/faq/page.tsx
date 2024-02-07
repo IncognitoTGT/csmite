affdd905ffae305016af9abd162e2ba0a48e3875
incognitotgt <mashoor539@icloud.com>
2024-02-07 12:02:57 -0500

    faq

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -8,7 +8,7 @@ export default function FAQ() {
         <div className="flex text-3xl text-mauve font-bold font-mono justify-center items-center">
           FAQ
         </div>
-        <div className="flex flex-col text-lg text-text font-sans leading-loose justify-center w-[1000px] items-center mt-6 pb-5 mx-auto space-y-2">
+        <div className="flex flex-col text-lg text-text font-sans leading-loose justify-center w-[1000px] items-center mt-6 pb-5 mx-auto space-y-2 text-center">
           <h1 className="text-2xl font-sans font-bold text-mauve">
             I don&apos;t have a flashdrive, can you send me one?
           </h1>
@@ -27,14 +27,20 @@ export default function FAQ() {
             Download
           </Link>
           <h1 className="text-2xl font-sans font-bold text-mauve">
-            Why are ARM boards not currently supported?
+            Why is my board grayed out?
           </h1>
+          You have an ARM device.
+          <br />
           A built in ChromeOS function does not work on ARM, and support will
           take a lot of time.
           <h1 className="text-2xl font-sans font-bold text-mauve">
             I&apos;m a panicking sysadmin
           </h1>
           calm down and don&apos;t worry about this.
+          <h1 className="text-2xl font-sans font-bold text-mauve">
+            Why is my board not supported?
+          </h1>
+          There wasn&apos;t a leaked shim for your device. Threaten your Chromebook manufacturer (don&apos;t actually do this).
         </div>
       </main>
     </>
